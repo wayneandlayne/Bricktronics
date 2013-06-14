949f60b27b5c6d75e6ce602428f985ae592e08f9
Matthew Beckler <matthew.beckler@gmail.com>
2013-06-14 11:28:54 -0400

    added inline comments back into the three sketches for the gripper bot code

--- software/BricktronicsMegashield/examples/Gripperbot/Base/Base.ino
+++ software/BricktronicsMegashield/examples/Gripperbot/Base/Base.ino
@@ -5,13 +5,11 @@
 // Chapter 7: GripperBot: Tank Base
 // Website: http://www.wayneandlayne.com/bricktronics
 
-// This file was originally commented for print.  It will be recommented inline shortly, and the example will be updated in a new library update.
-
 BricktronicsMegashield brick = BricktronicsMegashield();
 Motor r = Motor(&brick, 1);
 Motor l = Motor(&brick, 2);
 
-void setup()
+void setup() // The setup() function runs once at startup.
 {
     Serial.begin(9600);
     brick.begin();
@@ -19,38 +17,39 @@ void setup()
     l.begin();
 }
 
-void process_incoming_command(char cmd, char arg0)
+void process_incoming_command(char cmd, char arg0) // takes in the two bytes of the body of a command, and then performs the specified action.
 {
     int speed = 0;
     switch (cmd)
     {
-    case '6':
-    case 6:
-        speed = arg0*2;
-        l.set_speed(speed);
-        break;
-    case '7':
-    case 7:
-        speed = arg0*2;
-        r.set_speed(speed);
-        break;
-    default:
-        break;
+        case '6':
+        case 6: // If the command was "set the speed to motor 6 (left tank tread)," then the code that follows is run.
+            speed = arg0*2;
+            l.set_speed(speed);
+            break;
+        case '7':
+        case 7: // If the command was "set the speed to motor 7 (right tank tread)," the code that follows is run.
+            speed = arg0*2;
+            r.set_speed(speed);
+            break;
+        default:
+            break;
     }
 }
 
-void loop() {
+void loop()
+{
     while (Serial.available() < 3)
     {
         //Do nothing while we wait for a full command to come in.
     }
-    char start = Serial.read();
-    if (start != '*')
+    char start = Serial.read(); // Remove the oldest character from the serial buffer, and put it into start.
+    if (start != '*') // This isn't the start of the command, so let's try the next one.
     {
-        return;
+        return; // Start back over at the top of loop().
     }
 
-    char cmd = Serial.read();
+    char cmd = Serial.read(); // Read the command and argument, and call process_incoming_command().
     char arg0 = Serial.read();
     process_incoming_command(cmd, arg0);
 }
