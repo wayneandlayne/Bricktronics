f01dff66c16807f16cd02a9438c6e38d1d327be9
Matthew Beckler <matthew.beckler@gmail.com>
2014-06-11 09:52:13 -0500

    cleaned up color lamp example

--- software/Bricktronics/examples/Basics/ColorLamp/ColorLamp.ino
+++ software/Bricktronics/examples/Basics/ColorLamp/ColorLamp.ino
@@ -1,32 +1,72 @@
+// Bricktronics Example: ColorLamp
+// http://www.wayneandlayne.com/bricktronics
+// This example uses a LEGO NXT Color Sensor.
+//
+// The Color sensor can also act like a red, green, or blue lamp.
+// This example switches between these three colors in turn.
+
+
+// Include the Bricktronics ColorSensor library
+#include <ColorSensor.h>
+
+
+// This example can be run in three different ways. Pick one, and un-comment
+// the code lines corresponding to your chosen method. Comment-out the lines
+// for the other methods that you aren't using.
+
+// 1. With a Bricktronics Shield - Include these lines and be sure to
+// call Bricktronics::begin() in the setup() function below.
+// Select the sensor port for the ColorSensor (BS_SENSOR_3 and BS_SENSOR_4) below.
+// Use the jumpers to connect only pins 3-4 for the color sensor.
+//
 #include <Wire.h>
 #include <Bricktronics.h>
+ColorSensor c = ColorSensor(Bricktronics::BS_SENSOR_3);
 
-//Bricktronics Example: ColorLamp
-//http://www.wayneandlayne.com/bricktronics
-//This example uses a color sensor.
+// 2. With a Bricktronics Megashield - Include these lines but do not
+// call Bricktronics::begin() in the setup() function below.
+// Select the sensor port for the ColorSensor (BMS_SENSOR_1 through BMS_SENSOR_4) below.
+// Use the jumpers to connect only pins 3-4 for the color sensor.
+//
+//#include <Bricktronics.h>
+//ColorSensor c = ColorSensor(Bricktronics::BMS_SENSOR_1);
 
-//The Color Sensor can also act like a red, green, or blue lamp.  This example switches between them.
+// 3. With a Bricktronics Breakout board - No additional includes needed, just
+// update the pin assignments in the ColorSensor constructor below.
+//
+// The ColorSensor() arguments are: clockPin, dataPin
+// There are a few considerations for pin assignments:
+// dataPin - This must be an analog pin.
+//
+//ColorSensor c = ColorSensor(8, 16);
 
-//Connect a Color Sensor into Sensor Port 3.  Make sure to adjust the jumpers on Sensor Port 3 so that only pins 3 and 4 are connected.  There's an image of this on the Bricktronics website.
-
-Bricktronics brick = Bricktronics();
-ColorSensor color = ColorSensor(&brick, 3); //Plug a Color Sensor into Sensor Port 3, and adjust the jumpers so that only pins 3 and 4 are connected.
 
 void setup()
 {
-    Serial.begin(115200);
-    brick.begin();
-    color.begin();
+  // Be sure to set your serial console to 115200 baud.
+  Serial.begin(115200);
+
+  // Only call this if you are using a Bricktronics Shield,
+  // otherwise leave it commented-out.
+  Bricktronics::begin();
+
+  // Initialize the color sensor connections
+  c.begin();
 }
+
 
 void loop()
 {
-    color.begin(TYPE_COLORRED);
-    delay(500);
-    color.begin(TYPE_COLORBLUE);
-    delay(500);
-    color.begin(TYPE_COLORGREEN);
-    delay(500);
-    color.begin(TYPE_COLORNONE);
-    delay(500);
+  c.begin(TYPE_COLORRED);
+  delay(500);
+
+  c.begin(TYPE_COLORBLUE);
+  delay(500);
+
+  c.begin(TYPE_COLORGREEN);
+  delay(500);
+
+  c.begin(TYPE_COLORNONE);
+  delay(500);
 }
+
